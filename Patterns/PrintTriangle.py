7e98d47a367688bb92aedab9114431c9dcc0414e
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-08-24 18:39:05 +0300

    Print Triangle -GFG

--- Patterns/PrintTriangle.py
+++ Patterns/PrintTriangle.py
@@ -2,6 +2,8 @@ N = 5
 
 def PrintTriangle(N):
     for i in range(N):
-        for j in range(N):
-            print('* ' * 5, end='' )
+        for j in range(i + 1):
+            print('* ', end='')
         print('')
+
+PrintTriangle(N)
