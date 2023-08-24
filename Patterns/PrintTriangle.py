1bca286e28bd6b2370f56dd55f9220d86b0ef434
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-08-24 19:51:02 +0300

    Print Triangle In Dups - GFG

--- Patterns/PrintTriangle.py
+++ Patterns/PrintTriangle.py
@@ -17,4 +17,13 @@ def PrintTriangleInNums(N):
         print('')
 
 
-PrintTriangleInNums(N)
+# PrintTriangleInNums(N)
+
+
+def PrintTriangleInDups(N):
+    for i in range(1, N + 1):
+        for j in range(1, i + 1):
+            print(i, end=' ')
+        print()
+
+PrintTriangleInDups(N)
