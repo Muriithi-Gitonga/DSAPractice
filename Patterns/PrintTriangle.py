00257151b88e944f17bb89b7e2694311b741a27b
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-08-24 22:14:11 +0300

    Print Isosceles Triangle - GFG

--- Patterns/PrintTriangle.py
+++ Patterns/PrintTriangle.py
@@ -26,4 +26,43 @@ def PrintTriangleInDups(N):
             print(i, end=' ')
         print()
 
-PrintTriangleInDups(N)
+# PrintTriangleInDups(N)
+
+
+def printTriangleRev(N):
+    # Code here
+    for i in range(N + 1, 1, -1):
+        for j in range(1, i):
+            print('*', end=' ')
+        print()
+
+# printTriangleRev(N)
+
+
+def printTriangleRevNums(N):
+    for i in range(N + 1, 1, -1):
+        for j in range(1, i):
+            print(j, end=' ')
+        print()
+
+
+# printTriangleRevNums(N)
+
+
+def PrintIsoscelesTriangle(N):
+    for i in range(1, N + 1):
+
+        for j in range(N - i):
+            print(" ", end='')
+        
+        for k in range(2 * i - 1):
+            print('*', end='')
+
+        for l in range(N - i):
+            print(" ", end='')
+        
+        print()
+
+PrintIsoscelesTriangle(N)
+
+        
