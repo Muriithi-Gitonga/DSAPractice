fdf774bd0e9b0ce7f00857f3a0398991aee2035e
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-08-24 19:11:29 +0300

    Print Triangle In Nums - GFG

--- Patterns/PrintTriangle.py
+++ Patterns/PrintTriangle.py
@@ -1,9 +1,20 @@
 N = 5
 
+
 def PrintTriangle(N):
     for i in range(N):
         for j in range(i + 1):
             print('* ', end='')
         print('')
 
-PrintTriangle(N)
+# PrintTriangle(N)
+
+
+def PrintTriangleInNums(N):
+    for i in range(N):
+        for j in range(i + 1):
+            print(f'{j + 1} ', end='')
+        print('')
+
+
+PrintTriangleInNums(N)
